fcc09b7412df2abb47d31df022d1d40e652aed7c
Alexander Zaitsev <alexzaitsev@users.noreply.github.com>
2017-10-26 14:46:32 +0300

    Create BirthdayCakeCandles.java

---
+++ Algorithms/Warmup/Birthday Cake Candles/BirthdayCakeCandles.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int birthdayCakeCandles(int n, int[] ar) {
+        OptionalInt highest = Arrays.stream(ar).max();
+        long count = highest.isPresent() ? Arrays.stream(ar).filter(i -> i == highest.getAsInt()).count() : 0l;
+        return Math.toIntExact(count);
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        int result = birthdayCakeCandles(n, ar);
+        System.out.println(result);
+    }
+}
