0a9da84fb26eb64c7e41415789f6011229d44527
Alex Zaitsev <alexzaitsev@users.noreply.github.com>
2017-10-27 19:04:06 +0300

    Create DivisibleSumPairs.java

---
+++ Algorithms/Implementation Challenges/Divisible Sum Pairs/DivisibleSumPairs.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int divisibleSumPairs(int n, int k, int[] ar) {
+        int count = 0;
+        for (int i=0; i<n-1; i++) {
+            for (int j=i+1; j<n; j++) {
+                if ((ar[i] + ar[j]) % k == 0) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int k = in.nextInt();
+        int[] ar = new int[n];
+        for(int ar_i = 0; ar_i < n; ar_i++){
+            ar[ar_i] = in.nextInt();
+        }
+        System.out.println(divisibleSumPairs(n, k, ar));
+    }
+}
