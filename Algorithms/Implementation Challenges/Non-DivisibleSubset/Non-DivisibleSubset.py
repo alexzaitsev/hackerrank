22d25174fd06700c3596f3252b9ae5dd80ccde97
Alex Zaitsev <alexzaitsev@users.noreply.github.com>
2017-10-28 15:31:22 +0300

    Create Non-DivisibleSubset.py

---
+++ Algorithms/Implementation Challenges/Non-DivisibleSubset/Non-DivisibleSubset.py
@@ -0,0 +1,17 @@
+n, k = map(int, input().strip().split())
+numbers = list(map(int, input().strip().split()))
+
+counts = [0]*k
+for number in numbers:
+    counts[number%k] += 1
+
+count = min(counts[0], 1)
+for i in range(1, k//2 + 1):
+    if i != k-i:
+        count += max(counts[i], counts[k-i])
+        
+if k%2 == 0:
+    count += 1
+    
+print(count)
+    
