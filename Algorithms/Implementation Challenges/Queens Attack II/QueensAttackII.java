93f14f067d4eb978fdf734b55fd24dbce6c75dec
Alex Zaitsev <alexzaitsev@users.noreply.github.com>
2017-10-30 23:23:18 +0200

    Update QueensAttackII.java

--- Algorithms/Implementation Challenges/Queens Attack II/QueensAttackII.java
+++ Algorithms/Implementation Challenges/Queens Attack II/QueensAttackII.java
@@ -1,5 +1,3 @@
-// Memory-consuming solution, not all tests were passed
-
 import java.io.*;
 import java.util.*;
 import java.text.*;
@@ -7,10 +5,14 @@ import java.math.*;
 import java.util.regex.*;
 
 public class Solution {
-
-    static int check(int[][] board, int size, int rQueen, int cQueen, int xStep, int yStep) {
+    
+    static int check(Map<Integer, List<Integer>> obstacles, int size, int rQueen, int cQueen, int xStep, int yStep) {
         int count = 0;
-        while(rQueen > 0 && rQueen < size && cQueen > 0 && cQueen < size && board[rQueen][cQueen] == 0) {
+        while(rQueen > 0 && rQueen < size && cQueen > 0 && cQueen < size) {
+            List<Integer> columns = obstacles.get(rQueen);
+            if (columns != null && columns.contains(cQueen)) {
+                break;
+            }
             rQueen += yStep;
             cQueen += xStep;
             count++;
@@ -25,24 +27,29 @@ public class Solution {
         int rQueen = in.nextInt();
         int cQueen = in.nextInt();
         int size = n + 1;
-        int[][]board = new int[size][size];
+        Map<Integer, List<Integer>> obstacles = new HashMap<>();
         for(int i = 0; i < k; i++){
             int rObstacle = in.nextInt();
             int cObstacle = in.nextInt();
-            board[rObstacle][cObstacle] = 1;
+            if (!obstacles.containsKey(rObstacle)) {
+                List<Integer> columns = new ArrayList<>();
+                columns.add(cObstacle);
+                obstacles.put(rObstacle, columns);
+            } else {
+                obstacles.get(rObstacle).add(cObstacle);
+            }
         }
         
         int count = 0;
-        count += check(board, size, rQueen, cQueen + 1, 1, 0);
-        count += check(board, size, rQueen, cQueen - 1, -1, 0);
-        count += check(board, size, rQueen - 1, cQueen, 0, -1);
-        count += check(board, size, rQueen + 1, cQueen, 0, 1);
-        count += check(board, size, rQueen + 1, cQueen + 1, 1, 1);
-        count += check(board, size, rQueen + 1, cQueen - 1, -1, 1);
-        count += check(board, size, rQueen - 1, cQueen + 1, 1, -1);
-        count += check(board, size, rQueen - 1, cQueen - 1, -1, -1);
+        count += check(obstacles, size, rQueen, cQueen + 1, 1, 0);
+        count += check(obstacles, size, rQueen, cQueen - 1, -1, 0);
+        count += check(obstacles, size, rQueen - 1, cQueen, 0, -1);
+        count += check(obstacles, size, rQueen + 1, cQueen, 0, 1);
+        count += check(obstacles, size, rQueen + 1, cQueen + 1, 1, 1);
+        count += check(obstacles, size, rQueen + 1, cQueen - 1, -1, 1);
+        count += check(obstacles, size, rQueen - 1, cQueen + 1, 1, -1);
+        count += check(obstacles, size, rQueen - 1, cQueen - 1, -1, -1);
         
         System.out.println(count);
     }
 }
-
