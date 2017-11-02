b35a4a3805c3f7fa0fd415ef667ce8a7a8dd2841
Alex Zaitsev <alexzaitsev@users.noreply.github.com>
2017-11-02 14:40:09 +0200

    Create LinkedListsDetectaCycle.py

---
+++ Cracking the Coding Interview/Linked Lists- Detect a Cycle/LinkedListsDetectaCycle.py
@@ -0,0 +1,29 @@
+"""
+Detect a cycle in a linked list. Note that the head pointer may be 'None' if the list is empty.
+
+A Node is defined as: 
+ 
+    class Node(object):
+        def __init__(self, data = None, next_node = None):
+            self.data = data
+            self.next = next_node
+"""
+
+
+def has_cycle(head):
+    if head == None:
+        return 0
+    
+    traversed = [0]*100;
+    counter = 1
+    nextOne = head.next
+    
+    while True:
+        if nextOne == None:
+            return 0
+        if nextOne is head:
+            return 1 - min(1, sum(traversed))
+        traversed[counter] += 1
+        counter += 1
+        nextOne = nextOne.next
+        print(traversed)
