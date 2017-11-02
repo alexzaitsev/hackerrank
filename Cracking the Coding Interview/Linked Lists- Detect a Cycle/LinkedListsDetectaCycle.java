39712a018754cbf5135de52136f8e244eec47529
Alex Zaitsev <alexzaitsev@users.noreply.github.com>
2017-11-02 15:32:20 +0200

    Create LinkedListsDetectaCycle.java

---
+++ Cracking the Coding Interview/Linked Lists- Detect a Cycle/LinkedListsDetectaCycle.java
@@ -0,0 +1,23 @@
+// https://www.hackerrank.com/challenges/ctci-linked-list-cycle/problem
+
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'null' if the list is empty.
+
+A Node is defined as: 
+    class Node {
+        int data;
+        Node next;
+    }
+*/
+
+boolean hasCycle(Node head) {
+    Node fast = head;
+    while (head != null && fast != null) {
+        fast = fast.next.next;
+        head = head.next;
+        if (fast != null && head != null && fast.equals(head)) {
+            return true;
+        }
+    }
+    return false;
+}
