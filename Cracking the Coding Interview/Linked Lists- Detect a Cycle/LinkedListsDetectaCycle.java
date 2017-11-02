25c0f16fd6d796ff67eb4dc5df702b74f55c7d5a
Alex Zaitsev <alexzaitsev@users.noreply.github.com>
2017-11-02 15:38:30 +0200

    Update LinkedListsDetectaCycle.java

--- Cracking the Coding Interview/Linked Lists- Detect a Cycle/LinkedListsDetectaCycle.java
+++ Cracking the Coding Interview/Linked Lists- Detect a Cycle/LinkedListsDetectaCycle.java
@@ -12,7 +12,7 @@ A Node is defined as:
 
 boolean hasCycle(Node head) {
     Node fast = head;
-    while (head != null && fast != null) {
+    while (head != null && fast != null && fast.next != null) {
         fast = fast.next.next;
         head = head.next;
         if (fast != null && head != null && fast.equals(head)) {
